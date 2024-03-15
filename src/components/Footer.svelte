80d0058a7fad2047007c92d558bd4510cb8384a7
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2024-03-15 14:45:12 -0700

    add Header and Footer components

---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<footer>
+  <div class="copyright">© 2024</div>
+</footer>
+
+<style>
+  footer {
+    padding: 40px;
+    text-align: center;
+  }
+
+  .copyright {
+    border-top: 1px solid #ddd;
+    color: #aaa;
+    display: inline-block;
+    font-size: 14px;
+    padding: 20px;
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<header>
+  <h1>
+    <img
+      src="/src/assets/star-wars-jedi.svg"
+      alt="Star Wars Jedi Order Emblem"
+    />
+  </h1>
+</header>
+
+<style>
+  header {
+    background: #eeeeee;
+    padding: 20px;
+  }
+
+  h1 {
+    margin: 0;
+    text-align: center;
+  }
+
+  img {
+    max-width: 100px;
+  }
+</style>
